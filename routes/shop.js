ecd44797e513394f4e9f4b74b46beaa54e7c260a
RinaSreiber <r0583267823@gamil.com>
2023-05-17 01:56:05 +0300

    without conflicts

--- routes/shop.js
+++ routes/shop.js
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 const express = require('express')
 const router = express.Router()
 const { prods_ar } = require('../data/UsersData')
@@ -42,43 +42,3 @@ router.get("/category", (req, res) => {
 })
 
 module.exports = router
-=======
-const express = require('express');
-const router = express.Router();
-const {prods_ar} = require("../Data/ShopData")
-router.get('/',(req,res)=>{
-    res.json(prods_ar)
-})
-router.get("/category",(req,res)=>{
-    let categoryQ=req.query.category
-    let temp_ar=prods_ar.filter(item=>{
-        return item.cat == categoryQ
-    })
-
-    res.json(temp_ar)
-})
-const Shop = require("../ShopProfile")
-// הוספה של מוצר חדש
-router.post("/",(req,res)=>{
-    prods_ar.push(req.body)
-    res.send(prods_ar)
-})
-//עדכון של מוצר חדש
-router.put("/",(req,res)=>{
-    prods_ar.forEach(Element=>{
-       if (Element.id==req.body.id)
-        Element=req.body.id;
-        res.send(prods_ar)
-    })
-})
-//מחיקה של מוצר
-router.delete("/",(req,res)=>{
-    prods_ar.forEach(Element=>{
-       if (Element.id==req.body.id)
-       prods_ar.delete(Element)
-        res.send(prods_ar)
-    })
-   
-})
-module.exports =router;
->>>>>>> 0674819c8848eb3a8b403d216db7f5e0ae2e2f53
